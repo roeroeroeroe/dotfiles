3b2382e0cfc2982cef95cf5a79a364060bd76245
roe <fake@any.com>
2025-01-11 05:45:27 +0300

    update configs

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -5,16 +5,16 @@ _rainbow() {
 	[[ "$TERM" != *256color* ]] && { print "$str"; return; }
 
 	len=${#str}
-	start=$(( RANDOM % (256 - len) + 1 ))
+	start=$(( (RANDOM % (256 - len)) + 1 ))
 
-	for (( i = 1; i <= len; i++ )); do
-		color="%F{$(( (start + i - 1) % 256 ))}"
-		print -n "$color${str[i]}"
+	for (( i = 0; i < len; i++ )); do
+		color=$(( ((start + i - 1) % 255) + 1 ))
+		print -n "%F{$color}${str[i+1]}"
 	done
 
 	print "%f"
 }
-_host=$(_rainbow $HOST)
+_user=$(_rainbow $USER)
 
 setopt prompt_subst
 zstyle ":vcs_info:*" check-for-changes true
@@ -25,8 +25,7 @@ precmd() {
 	vcs_info
 	local git_branch=""
 	[ -n "$vcs_info_msg_0_" ] && git_branch="%F{white} [$vcs_info_msg_0_%f%F{white}]%f"
-	# PS1="$_newline%F{white}%~%f$git_branch%f%F{white} [%n@%M] [%T]%(1j. [%j].)%f%F{$_yellow}$_newline>%f "
-	PS1="$_newline%F{white}%~%f$git_branch%f%F{white} [%n@$_host%F{white}] [%T]%(1j. [%j].)%f%F{$_yellow}$_newline>%f "
+	PS1="$_newline%F{white}%~%f$git_branch%f%F{white} [$_user%F{white}@%M] [%T]%(1j. [%j].)%f%F{$_yellow}$_newline>%f "
 	RPROMPT="%(?..%F{white}%?%f) "
 }
 PS2="%F{white}>%f "
