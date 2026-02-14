d755be2f4590c8f3adabf30d3b6008bebc8de5ee
roe <fake@any.com>
2026-02-14 19:52:25 +0300

    update configs

--- config/nvim/lua/cfg/plugins/treesitter.lua
+++ config/nvim/lua/cfg/plugins/treesitter.lua
@@ -1,40 +1,36 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"css",
-				"go",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"nginx",
-				"python",
-				"sql",
-				"typescript",
-				"xml",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
+		local ts = require("nvim-treesitter")
+		local parsers = {
+			'bash',
+			'c',
+			'cpp',
+			'css',
+			'go',
+			'html',
+			'javascript',
+			'json',
+			'lua',
+			'markdown',
+			'nginx',
+			'python',
+			'sql',
+			'typescript',
+			'xml',
+			'zsh',
+		}
+		ts.install(parsers):wait(300000)
+
+		local patterns = vim.list_extend(vim.deepcopy(parsers), {
+			'sh'
+		})
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = patterns,
+			callback = function()
+				vim.treesitter.start()
+			end,
 		})
 	end,
 }
